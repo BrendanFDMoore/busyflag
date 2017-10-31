0053178ce0c2dab5ca86e4ef567eab693641c577
Brendan Moore <brendan.moore@rangle.io>
2017-10-31 01:04:20 -0400

    Finish initial verison of HypeFive, add sound

--- hypefive.js
+++ hypefive.js
@@ -1,7 +1,8 @@
 const five = require("johnny-five");
+const _ = require("lodash");
 // const sleep = require('sleep');
 
-console.log('HypeFive v0.2');
+console.log('HypeFive v1.0');
 
 let ARM_UP = false;
 let hangTimeout = null;
@@ -26,17 +27,84 @@ five.Board().on("ready", function() {
     pin: "A1"
   });
 
-  proximity.on("data", function() {
-    console.log("Proximity: ");
-    console.log("  cm  : ", this.cm);
-    console.log("  in  : ", this.in);
-    console.log("-----------------");
-  });
+  var piezo = new five.Piezo(3);
 
-  proximity.on("change", function() {
-    console.log("The obstruction has moved.");
-  });
+  const playQueueSound = () => {
+    piezo.play({
+      // song is composed by an array of pairs of notes and beats
+      // The first argument is the note (null means "no note")
+      // The second argument is the length of time (beat) of the note (or non-note)
+      song: [
+        ["C4", 1],
+        [null, 1],
+        ["C4", 1],
+        [null, 1],
+        ["C4", 1],
+      ],
+      tempo: 120
+    });
+  };
 
+  const playHappySound = () => {
+    piezo.play({
+      // song is composed by an array of pairs of notes and beats
+      // The first argument is the note (null means "no note")
+      // The second argument is the length of time (beat) of the note (or non-note)
+      song: [
+        ["G#5", 1],
+        ["F#5", 1],
+        ["G#5", 1],
+        ["A5", 1],
+      ],
+      tempo: 120
+    });
+  };
+
+  const playSadSound = () => {
+    piezo.play({
+      // song is composed by an array of pairs of notes and beats
+      // The first argument is the note (null means "no note")
+      // The second argument is the length of time (beat) of the note (or non-note)
+      song: [
+        ["A3", 1],
+        ["G3", 1],
+        ["F#3", 1],
+        ["F3", 2],
+      ],
+      tempo: 150
+    });
+  };
+
+  // playSadSound();  
+
+  const playGiveUp = () => {
+    piezo.play({
+      // song is composed by an array of pairs of notes and beats
+      // The first argument is the note (null means "no note")
+      // The second argument is the length of time (beat) of the note (or non-note)
+      song: [
+        ["C4", 1 / 4],
+        ["D4", 1 / 4],
+        ["F4", 1 / 4],
+        ["D4", 1 / 4],
+        ["A4", 1 / 4],
+        [null, 1 / 4],
+        ["A4", 1],
+        ["G4", 1],
+        [null, 1 / 2],
+        ["C4", 1 / 4],
+        ["D4", 1 / 4],
+        ["F4", 1 / 4],
+        ["D4", 1 / 4],
+        ["G4", 1 / 4],
+        [null, 1 / 4],
+        ["G4", 1],
+        ["F4", 1],
+        [null, 1 / 2]
+      ],
+      tempo: 100
+    });
+  };
   // const trigger = new five.Button({
   //   pin: 8,
   //   // type: "digital",
@@ -46,6 +114,7 @@ five.Board().on("ready", function() {
 
   const leftMeHanging = () => {
     console.log('leftMeHanging!');
+    playSadSound();
     moveArmDown();
   };
 
@@ -64,13 +133,16 @@ five.Board().on("ready", function() {
     if (ARM_UP) {
       return;
     }
-    moveArmUp();
-    hangTimeout = setTimeout(leftMeHanging, 3000);
+    playQueueSound();
+    // moveArmUp();
+    setTimeout(moveArmUp, 2500);
+    hangTimeout = setTimeout(leftMeHanging, 5000);
   };
 
   const highFive = () => {
+    clearTimeout(hangTimeout);
     console.log('highFive!');
-    cancelTimeout(hangTimeout);
+    playHappySound();
     moveArmDown();
   };
 
@@ -83,6 +155,45 @@ five.Board().on("ready", function() {
 
   forceSensor.on("low", function() {
     console.log('forceSensor low!');        
+  });
+
+
+  const proximityDetected = () => {
+    console.log('proximityDetected');
+    activateArm();
+  };
+
+  const debouncedProximityDetected = _.throttle(proximityDetected, 500, { leading: true });
+  
+  const proximityReading = (cm, inches) => {
+    // console.log("Proximity: ");
+    // console.log("  cm  : ", cm);
+    // console.log("  in  : ", inches);
+    // console.log("-----------------");
+
+    if (ARM_UP === false && cm >= 0.0 && cm <= 10.0) {
+      debouncedProximityDetected();
+    }
+  }
+
+  const throttledProximityReading = _.throttle(proximityReading, 500);
+
+  proximity.on("data", function() {
+    // console.log("Proximity: ");
+    // console.log("  cm  : ", this.cm);
+    // console.log("  in  : ", this.in);
+    // console.log("-----------------");
+    throttledProximityReading(this.cm, this.in);
+    // _.throttle(() => proximityReading(this.cm, this.in), 500);
+  });
+
+  const proximityObstructionMoved = () => {
+    // console.log("The obstruction has moved.");
+  }
+  const throttledProximityObstructionMoved = _.throttle(proximityObstructionMoved, 500);  
+
+  proximity.on("change", function() {
+    throttledProximityObstructionMoved();
   });
 
   // trigger.on("high", function() {
